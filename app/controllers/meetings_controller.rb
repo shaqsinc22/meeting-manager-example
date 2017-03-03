946f0171679e8f9a7612a21e0a89bc13bb6c3bdb
JackMarx <seriousfools@gmail.com>
2017-03-02 18:37:29 -0600

    first lecture

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -5,11 +5,38 @@ class MeetingsController < ApplicationController
     else
       @meetings = Meeting.all
     end
-    render 'index.html.erb'
+  end
+
+  def new
+    @meeting = Meeting.new
+    @tags = Tag.all
+  end
+
+  def create
+    @meeting = Meeting.new(
+                           name: params[:name],
+                           address: params[:address],
+                           start_time: params[:start_time],
+                           end_time: params[:end_time],
+                           notes: params[:notes]
+                           )
+
+    if @meeting.save
+      params[:tags].each do |tag_id|
+        MeetingTag.create(
+                          meeting_id: @meeting.id,
+                          tag_id: tag_id
+                          )
+      end
+      flash[:success] = "meeting successfully created"
+      redirect_to "/meetings"
+    else
+      @tags = Tag.all
+      render :new
+    end
   end
 
   def show
     @meeting = Meeting.find_by(id: params[:id])
-    render 'show.html.erb'
   end
 end
